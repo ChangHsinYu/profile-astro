b3b332925f4c0bbfdd6540fd61214cc002a22a2f
ChangHsinYu <eddyeddy5825945@gmail.com>
2026-06-27 13:55:02 +0200

    00

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
-// import { defineConfig } from "astro/config";
-// import react from "@astrojs/react";
-
-// export default defineConfig({
-//   site: "https://www.hsiinyu.com",
-//   integrations: [react()],
-//   devToolbar: {
-//     enabled: false
-//   }
-// });
-
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://changhsinyu.github.io",
-  base: "/profile-astro/"
+  site: "https://www.hsiinyu.com",
+  integrations: [react()],
+  devToolbar: {
+    enabled: false
+  }
 });
